33f734963ceff3162dd474c9c815e7dc4ab068ab
Akshatsonker <akshatsonker195@gmail.com>
2026-04-15 17:00:28 +0530

    fix: skip eslint during build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   serverExternalPackages: ["@trigger.dev/sdk"],
   images: {
     remotePatterns: [
